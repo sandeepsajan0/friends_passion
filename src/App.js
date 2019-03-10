e66e84c6904396c9fdf5133cde220728f8d62d29
sandeepsajan0 <sandeepsajan0@gmail.com>
2019-03-10 22:52:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AppData from "./AppData"
+import Data from "./Data"
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+class App extends React.Component{
+
+  render(){
+    const DataItem = AppData.map(item => <Data key={item.id} data={item}/>)
+    return(
+      <div className="Data-list">
+        {DataItem}
       </div>
-    );
+    )
   }
 }
 
---
+++ src/AppData.js
@@ -0,0 +1,39 @@
+const AppData = [
+	{
+		id: "1",
+		name: "Sandeep",
+		passion: "Cricket"
+	},
+	{
+		id:"2",
+		name:"Munish",
+		passion:"Marketing"
+	},
+	{
+		id:"3",
+		name:"Gaurav",
+		passion:"Bike Riding"
+	},
+	{
+		id:"4",
+		name:"Shubham",
+		passion:"Fitness"
+	},
+	{
+		id:"5",
+		name:"Anant",
+		passion:"Study"
+	},
+	{
+		id:"6",
+		name:"Gaurav Yadav",
+		passion:"Study"
+	},
+	{
+		id:"7",
+		name:"Lokinder",
+		passion:"Reasoning"
+	}
+]
+
+export default AppData
